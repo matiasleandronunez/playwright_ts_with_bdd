ee1050dc35615b4e48c3fd03dcf4573dce4e545f
matias <matias@example.com>
2024-07-06 19:07:32 -0300

    Removed unused env var and changed standard command to do bdd generation

--- run_regression.sh
+++ run_regression.sh
@@ -1,4 +1,3 @@
 export BASE_URL=https://practicesoftwaretesting.com/
 export BASE_API_URL=https://api.practicesoftwaretesting.com/
-export USE_GUI=true
-npx playwright test
+npx bddgen && npx playwright test --reporter=list --config=playwright.config.ts
