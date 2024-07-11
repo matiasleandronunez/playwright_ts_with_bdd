b35658efe5f3f5a56c2b08e01f3df8da88f97d6e
matias <matias@example.com>
2024-07-10 23:45:24 -0300

    Added product api search by id positive and negative scenario

--- features/backend/product.feature
+++ features/backend/product.feature
@@ -10,3 +10,13 @@ Feature: Product API
   Scenario: [PRO-002] User can get product by category
     Given I request a product to the Product API querying by category
     Then I find the requested product within the Product API response
+
+  @get_valid_product_by_id @PRO-003
+  Scenario: [PRO-003] User can get product by id
+    Given I request a product to the Product API querying by product id
+    Then I find the requested product within the Product API response
+
+  @get_invalid_product_by_id @PRO-004
+  Scenario: [PRO-004] User gets an appropriate error when getting non existing product by id
+    Given I request a product to the Product API querying with a non existing product id
+    Then I get an item not found response from the Product API response
--- steps/backend/product.steps.ts
+++ steps/backend/product.steps.ts
@@ -25,7 +25,7 @@ Given(/^I request a product to the Product API querying by brand$/, async ({ tes
     const brandId : string = brands.filter(b => b.slug === sampleProduct.brand.slug)[0].id;
 
     //Retrieve Product
-    console.log(`[${extractTestCaseTagID($tags)}] Using test product: ${sampleProduct.name} ( ${sampleProduct.id} )`);
+    console.log('\x1b[36m%s\x1b[0m',`\t[${extractTestCaseTagID($tags)}] Using test product: ${sampleProduct.name} ( ${sampleProduct.id} )`);
     const response = await request.get(CONFIG.baseApiHost + 'products',
                                     {
                                     headers: {
@@ -58,7 +58,7 @@ Given(/^I request a product to the Product API querying by category$/, async ({ 
     const categoryId : string = categories.filter(c => c.slug === sampleProduct.category.slug)[0].id;
 
     //Retrieve Product
-    console.log(`[${extractTestCaseTagID($tags)}] Using test product: ${sampleProduct.name} ( ${sampleProduct.id} )`);
+    console.log('\x1b[36m%s\x1b[0m',`\t[${extractTestCaseTagID($tags)}] Using test product: ${sampleProduct.name} ( ${sampleProduct.id} )`);
     const response = await request.get(CONFIG.baseApiHost + 'products',
         {
             headers: {
@@ -74,11 +74,54 @@ Given(/^I request a product to the Product API querying by category$/, async ({ 
     testContext.saveResponse(response);
 });
 
+Given(/^I request a product to the Product API querying by product id$/, async ({ testContext, request, $tags }) => {
+    const sampleProduct : Product = getRandomItem<Product>(productData);
+    testContext.storeData('product', sampleProduct);
+
+    //Retrieve Product
+    console.log('\x1b[36m%s\x1b[0m',`\t[${extractTestCaseTagID($tags)}] Using test product: ${sampleProduct.name} ( ${sampleProduct.id} )`);
+    const response = await request.get(CONFIG.baseApiHost + `products/${sampleProduct.id}`,
+        {
+            headers: {
+                'accept': 'application/json',
+            }
+        });
+
+    await expect(await response.status()).toBe(200);
+
+    testContext.saveResponse(response);
+});
+
+Given(/^I request a product to the Product API querying with a non existing product id$/, async ({ testContext, request, $tags }) => {
+
+    const response = await request.get(CONFIG.baseApiHost + `products/000000000000`,
+        {
+            headers: {
+                'accept': 'application/json',
+            }
+        });
+
+    testContext.saveResponse(response);
+});
 
 Then(/^I find the requested product within the Product API response$/, async ({ testContext }) => {
     const testProduct = testContext.getData('product');
-    const responseResults = (await (await testContext.retrieveResponse().json()).data).filter(d => d.name === testProduct.name);
+    const jsonResp = await (await testContext.retrieveResponse().json());
 
-    await expect(responseResults[0]).toBeDefined();
-    await expect(responseResults[0].name).toEqual(testProduct.name);
+    //Check if response contains array of products or just a single product
+    const responseResult : Product = (jsonResp.data !== undefined) ?
+        jsonResp.data.filter(d => d.name === testProduct.name)[0] : jsonResp;
+
+    await expect(responseResult).toBeDefined();
+    await expect(responseResult.name).toEqual(testProduct.name);
+    await expect(responseResult.description).toEqual(testProduct.description);
+    await expect(responseResult.price).toEqual(testProduct.price);
+    await expect(responseResult.is_location_offer).toEqual(testProduct.is_location_offer);
+    await expect(responseResult.is_rental).toEqual(testProduct.is_rental);
+});
+
+Then(/^I get an item not found response from the Product API response$/, async ({ testContext }) => {
+    const respStatus = await ((await testContext.retrieveResponse()).status());
+
+    await expect(respStatus).toBe(404);
 });
